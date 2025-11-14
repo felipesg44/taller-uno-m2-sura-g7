910e900305a267049ef0d465df109eb0bd31eaa3
felipesg44 <sierragomezandres@gmail.com>
2025-11-14 00:16:30 -0500

    Ejercicio 20: Reordenamiento de códigos usando sort, splice, unshift y reverse.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -151,3 +151,12 @@ tablero.fill("Casilla Vacía", 1, 5);
 
 console.log(tablero);
 
+//Ejercicio 20
+let codigos = ["A-10", "C-30", "B-20", "D-40"];
+
+codigos.sort();
+codigos.splice(2, 1); 
+codigos.unshift("X-99");
+codigos.reverse();
+
+console.log(codigos);
