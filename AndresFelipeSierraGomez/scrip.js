b6296d1381ceb7f67944206b624db609c138fb32
felipesg44 <sierragomezandres@gmail.com>
2025-11-13 22:00:48 -0500

    Ejercicio 2: Manipulación de lista de tareas eliminando y agregando elementos.

--- AndresFelipeSierraGomez/scrip.js
+++ AndresFelipeSierraGomez/scrip.js
@@ -6,3 +6,13 @@ compras.unshift("Huevos");
 compras.splice(1, 1);
 
 console.log(compras);
+
+//Ejercicio 2 
+let tareas = ["Revisar emails"];
+
+tareas.unshift("Llamar al cliente");
+tareas.unshift("Preparar reporte");
+tareas.shift();
+tareas.shift();
+
+console.log(tareas);
