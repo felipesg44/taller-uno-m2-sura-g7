bdd0249cf829ca26f0494289d3d8205ef282d860
felipesg44 <sierragomezandres@gmail.com>
2025-11-13 23:33:09 -0500

    Ejercicio 7: Reserva de asientos usando fill para actualizar rangos.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -49,3 +49,10 @@ mazo.reverse();
 mazo.shift();   
 
 console.log(mazo);
+
+//Ejercicio 7 
+let asientos = ["Ocupado", "Libre", "Libre", "Libre", "Ocupado"];
+
+asientos.fill("Reservado", 1, 4);
+
+console.log(asientos);
