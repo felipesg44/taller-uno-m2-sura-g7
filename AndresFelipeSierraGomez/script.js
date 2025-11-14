5c94cff7bcf7198eed449cca1f446094d430ac9e
felipesg44 <sierragomezandres@gmail.com>
2025-11-13 23:51:03 -0500

    Ejercicio 12: Gestión de versiones con push y pop simulando control de commits.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -86,3 +86,12 @@ carrito.unshift("Zapatos");
 carrito.splice(1, 1); 
 
 console.log(carrito);
+
+//Ejercicio 12 
+let commits = ["v1.0", "v1.1"];
+
+commits.push("v1.2");
+commits.pop();
+commits.push("v1.2-corregido");
+
+console.log(commits);
