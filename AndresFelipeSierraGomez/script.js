35dacf2ac80089b8d71e743142390506d7d6cddf
felipesg44 <sierragomezandres@gmail.com>
2025-11-13 23:53:18 -0500

    Ejercicio 13: Organización de pasos de una receta agregando inicio y fin.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -95,3 +95,11 @@ commits.pop();
 commits.push("v1.2-corregido");
 
 console.log(commits);
+
+//Ejercicio 13 
+let receta = ["Paso 2: Mezclar ingredientes", "Paso 3: Hornear"];
+
+receta.unshift("Paso 1: Pre-calentar horno");
+receta.push("Paso 4: Servir");
+
+console.log(receta);
