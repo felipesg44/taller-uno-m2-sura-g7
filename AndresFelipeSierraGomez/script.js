de4fcb26090612e6062ab5991ad299a02b74c2a7
felipesg44 <sierragomezandres@gmail.com>
2025-11-14 00:13:50 -0500

    Ejercicio 17: Actualización del feed agregando alerta y eliminando último elemento.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -127,3 +127,11 @@ parking.shift();
 parking.splice(0, 2, "MotoA", "MotoB");
 
 console.log(parking);
+
+//Ejercicio 17
+let feed = ["Foto de Ana", "Video de Juan", "Noticia de Empresa"];
+
+feed.unshift("Alerta de Sistema");
+feed.pop();
+
+console.log(feed);
