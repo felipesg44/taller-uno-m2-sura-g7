f376236dbae96efc724d5a1958cc6ba88eb720cf
felipesg44 <sierragomezandres@gmail.com>
2025-11-14 00:01:57 -0500

    Ejercicio 15: Actualización de equipo reemplazando miembros y agregando nuevos.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -111,3 +111,11 @@ palabras.reverse();
 palabras.splice(0, 1, "tres");
 
 console.log(palabras);
+
+//Ejercicio 15
+let equipo = ["Ana (Líder)", "Juan (Dev)", "Maria (Diseñadora)"];
+
+equipo.splice(1, 1); 
+equipo.push("Pedro (Dev)", "Luisa (Dev)");
+
+console.log(equipo);
