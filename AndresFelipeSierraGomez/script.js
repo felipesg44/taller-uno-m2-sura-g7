032e704ffae8b2916d5515379d907b3bdae30280
felipesg44 <sierragomezandres@gmail.com>
2025-11-13 23:27:57 -0500

    Ejercicio 5: Modificación de una fila reemplazando elementos con splice.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -33,3 +33,11 @@ colaImpresion.push("doc4.txt");
 colaImpresion.shift(); 
 
 console.log(colaImpresion);
+
+//Ejercicio 5 
+let fila = ["Ana", "Luis", "Carlos", "Maria"];
+
+fila.splice(1, 1); 
+fila.splice(1, 0, "Jorge"); 
+
+console.log(fila);
